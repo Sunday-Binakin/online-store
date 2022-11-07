567271cab237447aaf4b31d708d0ff00609d71ad
Sunday-Binakin <Sundaybinakin@gmail.com>
2022-11-07 23:29:04 +0000

    partials

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Online Store</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
-<body>
 
+<body>
+{{-- navbar --}}
+@include('partials.navbar')
+{{-- end of navbar --}}
+</nav>
 </body>
+
 </html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar-container">
+    <ul>
+        <li class="navbar-item">Home</li>
+        <li class="navbar-item">Women's</li>
+        <li class="navbar-item">men's</li>
+        <li class="navbar-item">pages</li>
+        <li class="navbar-item">blog</li>
+        <li class="navbar-item">contact</li>
+    </ul>
