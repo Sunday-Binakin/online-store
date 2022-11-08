eacf363ff60420c06aec6912dde802d22b9daf6a
Sunday-Binakin <Sundaybinakin@gmail.com>
2022-11-08 02:10:59 +0000

    flex

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Online Store</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/assets/css/styles.css') }}">
+    @vite(["public/assets/css/styles.css", "public/assets/js/script.js"])
 </head>
 
 <body>
-{{-- navbar --}}
-@include('partials.navbar')
-{{-- end of navbar --}}
-</nav>
+    {{-- navbar --}}
+    @include('partials.navbar')
+    {{-- end of navbar --}}
 </body>
 
 </html>
+
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,22 @@
-<nav class="navbar-container">
-    <ul>
-        <li class="navbar-item">Home</li>
-        <li class="navbar-item">Women's</li>
-        <li class="navbar-item">men's</li>
-        <li class="navbar-item">pages</li>
-        <li class="navbar-item">blog</li>
-        <li class="navbar-item">contact</li>
+<nav class="navbar">
+    {{-- <div class="conatiner"> --}}
+    <ul class="navbar-container">
+        <li class="navbar-item"><a>Home</a></li>
+        <li class="navbar-item"><a>Women's</a></li>
+        <li class="navbar-item"><a>men's</a></li>
+        <li class="navbar-item"><a>pages</a></li>
+        <li class="navbar-item"><a>blog</a></li>
+        <li class="navbar-item"><a>contact</a></li>
+        </ul>
+        {{--
+    </div> --}}
+        <ul class="other-items">
+            <div class="other-items">
+        <li><i class="navbar-item">Login/Register</i></li>
+        <li><i class="navbar-item">search</i></li>
+        <li><i class="navbar-item">love</i></li>
+        <li><i class="navbar-item">cart</i></li>
+    </div>
     </ul>
+</nav>
+
